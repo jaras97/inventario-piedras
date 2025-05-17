5e65a898440344a7d7f89d556411051c8c4dd308
jaras97 <mateojaras@gmail.com>
2025-05-16 20:20:26 -0500

    solucion redireccion dashboard

--- src/app/(dashboard)/categorias/page.tsx
+++ src/app/(dashboard)/categorias/page.tsx
@@ -2,16 +2,8 @@
 
 import { CategoryForm } from '@/components/components/CategoryForm';
 import { CategoryList } from '@/components/components/CategoryList';
-import { checkRole } from '@/lib/auth/checkRole';
-import { ROLES } from '@/lib/auth/roles';
-import { redirect } from 'next/navigation';
 
 export default async function CategoriasPage() {
-  const hasAccess = await checkRole([ROLES.ADMIN]); // 👈 SOLO ADMIN
-
-  if (!hasAccess) {
-    return redirect('/dashboard');
-  }
   return (
     <div className='p-6'>
       <h1 className='text-2xl font-bold mb-4'>Gestión de Categorías</h1>
